2c911aa28dcebfffd2c9a6294c70e7c86d2921da
kalvictors <kalvictors7@gmail.com>
2019-01-14 11:12:30 +0300

    my ride

---
+++ index.js
@@ -0,0 +1,7 @@
+function login(showhide){
+    if(showhide == "show"){
+        document.getElementById('login').style.visibility="visible";
+    }else if(showhide == "hide"){
+        document.getElementById('login').style.visibility="hidden"; 
+    }
+    }
